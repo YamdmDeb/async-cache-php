12636f0e0b4cf026b8457c978f74ecd5c03e9fc5
Serhii Cherneha <chernegasergiy3@gmail.com>
2026-01-26 01:36:17 +0200

    feat: Add Symfony Bundle for easy framework integration

---
+++ src/Bridge/Symfony/AsyncCacheBundle.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Fyennyi\AsyncCache\Bridge\Symfony;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Symfony Bundle for AsyncCache integration
+ */
+class AsyncCacheBundle extends Bundle
+{
+    public function getContainerExtension(): ?\Symfony\Component\DependencyInjection\Extension\ExtensionInterface
+    {
+        return new DependencyInjection\AsyncCacheExtension();
+    }
+}
