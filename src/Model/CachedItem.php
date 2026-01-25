84d3587cfb44a4621429b77c4e561d465ea9310c
Serhii Cherneha <chernegasergiy3@gmail.com>
2026-01-26 01:06:14 +0200

    feat: Add tagVersions field to CachedItem for logical invalidation

--- src/Model/CachedItem.php
+++ src/Model/CachedItem.php
@@ -9,12 +9,16 @@ class CachedItem
 {
     public const CURRENT_VERSION = 1;
 
+    /**
+     * @param array<string, string> $tagVersions Map of tag names to their versions at the moment of caching
+     */
     public function __construct(
         public readonly mixed $data,
         public readonly int $logicalExpireTime,
         public readonly int $version = self::CURRENT_VERSION,
         public readonly bool $isCompressed = false,
-        public readonly float $generationTime = 0.0
+        public readonly float $generationTime = 0.0,
+        public readonly array $tagVersions = []
     ) {
     }
 
