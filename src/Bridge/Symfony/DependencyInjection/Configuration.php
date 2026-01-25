7ff3ecfb3d2a4d988428f391d7435cd63e783248
Serhii Cherneha <chernegasergiy3@gmail.com>
2026-01-26 01:36:23 +0200

    feat: Add Symfony Configuration tree for AsyncCache

---
+++ src/Bridge/Symfony/DependencyInjection/Configuration.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Fyennyi\AsyncCache\Bridge\Symfony\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+class Configuration implements ConfigurationInterface
+{
+    public function getConfigTreeBuilder(): TreeBuilder
+    {
+        $treeBuilder = new TreeBuilder('async_cache');
+        $rootNode = $treeBuilder->getRootNode();
+
+        $rootNode
+            ->children()
+                ->scalarNode('rate_limiter_type')
+                    ->defaultValue('auto')
+                    ->validate()
+                        ->ifNotInArray(['auto', 'symfony', 'in_memory'])
+                        ->thenInvalid('Invalid rate limiter type %s')
+                    ->end()
+                ->end()
+                ->scalarNode('default_strategy')
+                    ->defaultValue('strict')
+                ->end()
+            ->end();
+
+        return $treeBuilder;
+    }
+}
