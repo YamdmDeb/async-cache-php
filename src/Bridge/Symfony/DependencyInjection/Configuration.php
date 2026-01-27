4be0327188e1e60dd846ba10defa8f39d6124d77
Serhii Cherneha <chernegasergiy3@gmail.com>
2026-01-27 20:07:48 +0200

    docs: Add license headers to all source files

--- src/Bridge/Symfony/DependencyInjection/Configuration.php
+++ src/Bridge/Symfony/DependencyInjection/Configuration.php
@@ -1,5 +1,28 @@
 <?php
 
+/*
+ *
+ *     _                          ____           _            ____  _   _ ____
+ *    / \   ___ _   _ _ __   ___ / ___|__ _  ___| |__   ___  |  _ \| | | |  _ \
+ *   / _ \ / __| | | | '_ \ / __| |   / _` |/ __| '_ \ / _ \ | |_) | |_| | |_) |
+ *  / ___ \\__ \ |_| | | | | (__| |__| (_| | (__| | | |  __/ |  __/|  _  |  __/
+ * /_/   \_\___/\__, |_| |_|\___|\____\__,_|\___|_| |_|\___| |_|   |_| |_|_|
+ *              |___/
+ *
+ * This program is free software: you can redistribute and/or modify
+ * it under the terms of the CSSM Unlimited License v2.0.
+ *
+ * This license permits unlimited use, modification, and distribution
+ * for any purpose while maintaining authorship attribution.
+ *
+ * The software is provided "as is" without warranty of any kind.
+ *
+ * @author Serhii Cherneha
+ * @link https://chernega.eu.org/
+ *
+ *
+ */
+
 namespace Fyennyi\AsyncCache\Bridge\Symfony\DependencyInjection;
 
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
