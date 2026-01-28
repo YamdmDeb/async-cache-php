3b55dfac6ff41fd71fd570d0c14f883f7852ba13
Serhii Cherneha <chernegasergiy3@gmail.com>
2026-01-28 18:22:18 +0200

    build: Add PHP CS Fixer configuration

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,17 +1,47 @@
 <?php
 
-$finder = PhpCsFixer\Finder::create()
+use PhpCsFixer\Config;
+use PhpCsFixer\Finder;
+
+$finder = Finder::create()
     ->in(__DIR__ . '/src')
-    ->in(__DIR__ . '/tests');
+    ->name('*.php');
 
-$config = new PhpCsFixer\Config();
-
-return $config->setRules([
-    'ordered_imports' => ['sort_algorithm' => 'alpha'],
-    'no_whitespace_in_blank_line' => true,
-    'no_trailing_whitespace' => true,
-    'no_unused_imports' => true,
-    'single_blank_line_at_eof' => true,
-])
-    ->setLineEnding("\n")
-    ->setFinder($finder);
+return (new Config())
+    ->setRiskyAllowed(true)
+    ->setFinder($finder)
+    ->setRules([
+        '@PSR12' => true,
+        'no_whitespace_in_blank_line' => true,
+        'no_trailing_whitespace' => true,
+        'single_blank_line_at_eof' => true,
+        'ordered_imports' => [
+            'sort_algorithm' => 'alpha',
+            'imports_order' => ['class', 'function', 'const'],
+        ],
+        'blank_line_between_import_groups' => false,
+        'no_unused_imports' => true,
+        'phpdoc_align' => [
+            'align' => 'vertical',
+        ],
+        'phpdoc_separation' => [
+            'groups' => [
+                ['template', 'inheritDoc'],
+                ['param', 'return', 'throws'],
+                ['see', 'link', 'since', 'deprecated'],
+            ],
+            'skip_unlisted_annotations' => false,
+        ],
+        'phpdoc_trim' => true,
+        'phpdoc_trim_consecutive_blank_line_separation' => true,
+        'phpdoc_indent' => true,
+        'no_superfluous_phpdoc_tags' => false,
+        'phpdoc_add_missing_param_annotation' => false,
+        'phpdoc_summary' => false,
+        'yoda_style' => [
+            'equal' => true,
+            'identical' => true,
+            'less_and_greater' => false,
+            'always_move_variable' => false,
+        ],
+    ]);
